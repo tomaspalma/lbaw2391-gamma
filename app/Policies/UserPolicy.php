d340c7b2e17e52acf097d53bd27e410bf60beba6
joaonevesf <joaodnfernandes99@gmail.com>
2023-11-18 17:27:43 +0000

    added user policy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    /**
+     * Determine whether the user can update the profile.
+     */
+    public function update(User $user, User $model): response
+    {
+        return ($user->id === $model->id || $user->is_admin) ? Response::allow() : Response::deny('You do not own this profile.');
+    }
+
+    /**
+     * Determine whether the user can update the password of a model.
+     */
+    public function updatePassword(User $user, User $model): response
+    {
+        return $user->id === $model->id ? Response::allow() : Response::deny('You do not own this profile.');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): response
+    {
+        return ($user->id === $model->id || $user->is_admin) ? Response::allow() : Response::deny('You do not own this profile.');
+    }
+
+}
