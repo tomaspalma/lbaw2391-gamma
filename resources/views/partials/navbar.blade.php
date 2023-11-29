f6ddae3eefb7ef0677c9aab0032be6901c8cab77
Tomás Palma <up202108880@edu.fe.up.pt>
2023-11-29 13:11:18 +0000

    fixed spelling mistake on notificiation counter of the notification bell

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -82,7 +82,7 @@
                         <div class="relative flex flex-row md:flex-col space-x-1 md:space-x-0">
                             <i class="hidden md:inline fa-solid fa-bell text-2xl"></i>
                             <span class="md:hidden">Notifications</span>
-                            <span id="notitication-counter" class="hidden text-xs md:absolute md:bottom-3 md:left-1.5 bg-red-500 text-white w-5 h-5 flex items-center justify-center rounded-full">1</span>
+                            <span id="notification-counter" class="hidden text-xs md:absolute md:bottom-3 md:left-1.5 bg-red-500 text-white w-5 h-5 flex items-center justify-center rounded-full">1</span>
                         </div>
                     </a>
                 </li>
