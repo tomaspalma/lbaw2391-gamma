55164ffd718af8b7d65c9153ca401f949f52a220
Tomás Palma <up202108880@edu.fe.up.pt>
2023-12-01 01:18:26 +0000

    Search on appeal dashboard is now working

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,18 +9,15 @@ use Illuminate\Auth\Access\Response;
 class UserPolicy
 {
     /**
-      * Determine whether or not the user has an app ban, e.g. if they have a ban to appeal from
-      */
-    public function can_appeal_appban(User $user): Response {
+     * Determine whether or not the user has an app ban, e.g. if they have a ban to appeal from
+     */
+    public function can_appeal_appban(User $user): Response
+    {
         $appban = AppBan::where('banned_user_id', $user->id)->get();
 
-        if(count($appban) > 0 && $appban[0]->appeal === null) {
-            return Response::allow();
-        } else if(count($appban) <= 0) {
-            return Response::deny("You do not have any app bans.");
-        } else if($appban[0]->appeal !== null) {
-            return Response::deny("You already appealed the ban and was not either accepted or rejected");
-        }
+        return count($appban) > 0
+            ? Response::allow()
+            : Response::deny("You do not have any app bans.");
     }
 
     /**
@@ -46,5 +43,4 @@ class UserPolicy
     {
         return ($user->id === $model->id || $user->is_admin()) ? Response::allow() : Response::deny('You do not own this profile.');
     }
-
 }
