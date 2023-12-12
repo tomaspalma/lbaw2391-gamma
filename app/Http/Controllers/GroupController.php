e23014e49fe15ef836a09a2cfa74a4be0d75d3e6
Davide64-dev <davidpcu@hotmail.com>
2023-12-12 17:16:04 +0000

    solved some problems

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -68,7 +68,7 @@ class GroupController extends Controller
             return redirect("/group/$id");
         } else {
 
-            $last_id = DB::select('SELECT id FROM users ORDER BY id DESC LIMIT 1')[0]->id;
+            $last_id = DB::select('SELECT id FROM group_request ORDER BY id DESC LIMIT 1')[0]->id;
             $new_id = $last_id + 1;
             DB::table('group_request')->insert([
                 'id' => $new_id,
