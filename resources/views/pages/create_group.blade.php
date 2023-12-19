84f78bbecc47d415c5ae264a1f3107071d28d27c
pfpo <pedro.oliveira2003g@gmail.com>
2023-12-19 18:57:57 +0000

    Update form validation feedback in create_group.blade.php

--- resources/views/pages/create_group.blade.php
+++ resources/views/pages/create_group.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.head')
 
+<head>
+    @vite(['resources/css/app.css', 'resources/js/group/create.js'])
+
+    <title>{{ config('app.name', 'Laravel') }} | Create Group</title>
+    <link href="{{ url('css/post.css') }}" rel="stylesheet">
+</head>
+
+
 <body>
     @include('partials.navbar')
 
@@ -18,19 +26,34 @@
                 <input type="file" name="banner" id="banner" class="hidden" onchange="document.getElementById('bannerPreview').src = window.URL.createObjectURL(this.files[0])">
                 <button type="button" class=" bg-black text-white m-2 px-4 py-2 rounded" onclick="document.getElementById('banner').click()">Upload Banner</button>
             </div>
-            <label for="name" class="text-sm text-gray-600">Group Name</label>
-            <input type="text" id="name" name="name" class="w-full border p-2 mb-4">
 
-            <label for="description" class="text-sm text-gray-600">Group Description</label>
-            <textarea id="description" name="description" class="w-full border p-2 mb-4"></textarea>
+            @error('image')
+                <p class="text-red-500 text-xs italic">{{ $message }}. Max size is 2mb.</p>
+            @enderror
+            @error('banner')
+                <p class="text-red-500 text-xs italic">{{ $message }}. Max size is 2mb.</p>
+            @enderror
 
-            <label for="privacy" class="text-sm text-gray-600">Privacy</label>
-            <select id="privacy" name="privacy" class="w-full border p-2 mb-4">
+            <label for="name" class="text-sm mt-2 text-gray-600">Group Name *</label>
+            <input type="text" id="name" name="name" value="{{ old('name') }}" class="w-full border p-2 mb-2">
+            @error('name')
+                <p class="text-red-500 text-xs italic">{{ $message }}</p> 
+            @enderror
+            <p id="name_error" class="text-red-500 text-xs italic hidden">Group name already taken.</p>
+
+            <label for="description" class="text-sm mt-2 text-gray-600">Group Description *</label>
+            <textarea id="description" name="description" value="{{ old('description') }}" class="w-full border p-2 mb-2"></textarea>
+            @error('description')
+                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+            @enderror
+
+            <label for="privacy" class="text-sm mt-2 text-gray-600">Privacy *</label>
+            <select id="privacy" name="privacy" class="w-full border p-2 mb-2">
                 <option value=0 selected>Public</option>
                 <option value=1>Private</option>
             </select>
 
-            <button type="submit" class="bg-black hover:bg-black text-white font-bold py-2 px-4 rounded">
+            <button type="submit" id="submit" class="bg-black hover:bg-black text-white font-bold py-2 px-4 rounded">
                 Submit
             </button>
             <a href="{{ route('feed') }}" class="bg-white hover:bg-gray-100 text-black hover:no-underline font-bold py-2 px-4 rounded">Cancel</a>
