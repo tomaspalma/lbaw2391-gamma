00374815af7ee6b0bd09b997af1e7e024633c525
joaonevesf <joaodnfernandes99@gmail.com>
2023-12-20 15:47:09 +0000

    added resize of image on file controller

--- resources/views/partials/post_card.blade.php
+++ resources/views/partials/post_card.blade.php
@@ -5,7 +5,7 @@
 <article data-entity="post" data-entity-id="{{$post->id}}" class="shadow-md post-card border border-black rounded-md my-4 p-6 cursor-pointer">
     <div class="flex align-middle justify-between space-x-4">
         <div class="flex space-x-4">
-            <img src="{{ $post->owner->getProfileImage() ?? 'hello'}}" class="rounded-full w-10 h-10" alt="{{ $post->owner->username }}'s Profile Image">
+            <img src="{{ $post->owner->getProfileImage('small') ?? 'hello'}}" class="rounded-full w-10 h-10" alt="{{ $post->owner->username }}'s Profile Image">
             <a class="hover:underline" href="{{ route('profile',['username' => $post->owner->username]) }}">
                 {{ $post->owner->username }} 
                 @auth
