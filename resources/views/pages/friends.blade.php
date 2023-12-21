03c36eb2854e1c46fb846fdf497f760f2d6ce9f8
joaonevesf <joaodnfernandes99@gmail.com>
2023-12-21 01:12:36 +0000

    friend_request is now a user_card

--- resources/views/pages/friends.blade.php
+++ resources/views/pages/friends.blade.php
@@ -21,7 +21,7 @@
 
 <main class="center mx-4">
     <div class="tab-container center justify-center flex border border-black rounded shadow my-4">
-        <div class="flex w-1/2 border-t-4 p-2 justify-center {{ $tab === 'friends' ? 'border-t-4 border-black' : '' }}">
+        <div class="flex w-1/2 p-2 justify-center {{ $tab === 'friends' ? 'border-t-4 border-black' : '' }}">
             <a href="/users/{{$user->username}}/friends" class="hover:underline text-lg font-bold">Friends</a>
         </div>
         @if(auth()->user() && auth()->user()->id === $user->id)
@@ -42,21 +42,8 @@
     @if($tab=='requests')
     <div>
         @foreach($friendRequests as $request)
-        <div class="flex justify-between items-center bg-white p-4 m-2 rounded shadow" id="request-{{$request->sender->username}}">
-            <div class="flex items-center space-x-4">
-                <img src="{{ $request->sender->getProfileImage('small') }}" alt="{{ $request->sender->username }}'s Profile Image" class="w-12 h-12 rounded-full">
-                <div>
-                    <a href="{{'/users/' . $request->sender->username}}">
-                        <h2 class="text-xl font-bold">{{ $request->sender->display_name }}</h2>
-                        <p class="text-gray-500">{{ $request->sender->username }}</p>
-                    </a>
-                </div>
-            </div>
-            <form id="friendRequestForm" data-username="{{ $request->sender->username }}" class="flex items-center space-x-4 my-auto" method="post">
-                @csrf
-                <button type="submit" name="action" value="accept" class="form-button-blue font-bold py-2 px-4 rounded">Accept</button>
-                <button type="submit" name="action" value="decline" class="form-button-red font-bold py-2 px-4 rounded">Decline</button>
-            </form>
+        <div id="request-{{$request->sender->username}}">
+        @include('partials.user_card', ['user' =>$request->sender, 'friendRequest' => true, 'adminView' => false])
         </div>
         @endforeach
     </div>
